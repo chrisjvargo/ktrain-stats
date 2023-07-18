4e05846cce55b78ce7f5a0e9ec4f309a8e27c9bc
chrisjvargo <christopherjvargo@gmail.com>
2023-07-17 19:48:34 -0600

    Update predictor.py

--- ktrain/tabular/predictor.py
+++ ktrain/tabular/predictor.py
@@ -157,7 +157,15 @@ class TabularPredictor(Predictor):
         # shap
         explainer = shap.KernelExplainer(f, df.iloc[:background_size, :])
         shap_values = explainer.shap_values(df_row, nsamples=nsamples, l1_reg="aic")
-        shap_dict = dict(zip(df_display_row.index, shap_values))
+        # Create a list of variable names
+        var_names = list(df_display_row.index)
+        
+        # If there are more shap_values than variable names, append placeholder names
+        if len(shap_values) > len(var_names):
+            var_names += ['var_{}'.format(i) for i in range(len(var_names), len(shap_values))]
+        
+        # Create a dictionary of shap values with corresponding variable names
+        shap_dict = dict(zip(var_names, shap_values))        
         expected_value = explainer.expected_value
 
         if not np.issubdtype(type(explainer.expected_value), np.floating):
